276bdc4e261f76c0f97f216dd68fde7b789e3cf8
Abrahan <abrahanramirez2410@gmail.com>
2025-05-20 13:17:50 -0400

    Especificaciones de la ventana

--- puzzle8.py
+++ puzzle8.py
@@ -4,3 +4,15 @@
 #--->LIBRERIAS<---
 import pygame #Interfaz Grafica
 import random #Aleatoriedad
+
+#--->CONFIGURACION GENERAL<---
+tamCasilla = 100 #Tamaño de la casilla
+tamTablero = 3 #Tamaño del tablero
+ancho = alto = tamCasilla * tamTablero #Ancho y alto del tablero
+FPS = 30 #Frames por segundo
+tamFuente = 48 #Tamaño de la fuente
+
+#--->COLORES<---
+blanco = (255, 255, 255)
+negro = (0, 0, 0)
+rojo = (197, 29, 52)
