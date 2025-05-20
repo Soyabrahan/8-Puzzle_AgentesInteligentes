3938c325cbd50b58f12f26bf1a6e6232a12497f1
Abrahan <abrahanramirez2410@gmail.com>
2025-05-20 13:13:54 -0400

    empezando el proyecto

---
+++ puzzle8.py
@@ -0,0 +1,6 @@
+#MiniProyecto de trabar con dos inteligencias artificales que resuelvan el puzle 8
+#usare la libreria pygame para la interfaz grafica
+
+#--->LIBRERIAS<---
+import pygame #Interfaz Grafica
+import random #Aleatoriedad
